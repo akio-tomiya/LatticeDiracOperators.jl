56f3087945f007df6fc6e9953a1f7cf667c42c10
cometscome <cometscome@gmail.com>
2026-02-07 23:13:33 +0900

    ad test

--- test/general/hmctest.jl
+++ test/general/hmctest.jl
@@ -11,6 +11,41 @@ import Gaugefields: Initialize_4DGaugefields
 using Test
 import Gaugefields.Temporalfields_module: Temporalfields, get_temp, unused!
 
+function make_μloop(Uout, C, D, E, μ, U, shift_μ, dim, t)
+    clear_U!(E)
+    for ν = μ:dim
+        if ν == μ
+            continue
+        end
+        shift_ν = ntuple(i -> ifelse(i == ν, 1, 0), dim)
+        _calc_action_step_addsum!(E, C, D, U[μ], U[ν], shift_μ, shift_ν)
+        #S += realtrace(Uout)
+    end
+    UTA = Traceless_AntiHermitian(E)
+    exptU!(Uout, UTA, t)
+end
+
+function _calc_action_step_addsum!(Uout, C, D, Uμ, Uν, shift_μ, shift_ν)
+    Uμ_pν = shift_U(Uμ, shift_ν)
+    Uν_pμ = shift_U(Uν, shift_μ)
+
+    mul!(C, Uμ, Uν_pμ)
+    mul!(D, C, Uμ_pν')
+    mul!(C, D, Uν')
+    add_U!(Uout, C)
+    #add_matrix!(Uout, C)
+    #S = realtrace(E)
+
+    mul!(C, Uν, Uμ_pν)
+    mul!(D, C, Uν_pμ')
+    mul!(C, D, Uμ')
+    add_U!(Uout, C)
+    #S += realtrace(E)
+
+    #return S
+end
+
+
 function shiftedadd(y, Uμ, x, γμ, shift_p, shift_m, phi1, phi2, κ)
     #U_n[ν](1 - γν) * ψ_{n + ν}
     mul_AshiftB!(phi1, Uμ, x, shift_p)
@@ -28,11 +63,35 @@ end
 #ψ_n - κ sum_ν U_n[ν](1 - γν)*ψ_{n+ν} + U_{n-ν}[-ν]^+ (1 + γν)*ψ_{n-ν}
 function apply_wilson!(y, U1, U2, U3, U4, x, params, phitemps, temp)
     U = (U1, U2, U3, U4)
+    C = temp[1]
+    D = temp[2]
+    Uout = temp[3]
+    Ufat1 = temp[4]
+    Ufat2 = temp[5]
+    Ufat3 = temp[6]
+    Ufat4 = temp[7]
+    E = temp[8]
+
 
     clear_fermion!(y)
     add_fermion!(y, 1, x)
     γs = (γ1, γ2, γ3, γ4)
     κ = params.κ
+
+    clear_U!(E)
+    Ufat = (Ufat1, Ufat2, Ufat3, Ufat4)
+    t = params.t
+    dim = 4
+
+    #=
+    for μ = 1:dim
+        shift_μ = ntuple(i -> ifelse(i == μ, 1, 0), dim)
+        make_μloop(Uout, C, D, E, μ, U, shift_μ, dim, t)
+        mul!(Ufat[μ], Uout, U[μ])
+        set_wing_U!(Ufat[μ])
+    end
+
+    =#
 
     phi1 = phitemps[3]
     phi2 = phitemps[4]
@@ -40,7 +99,9 @@ function apply_wilson!(y, U1, U2, U3, U4, x, params, phitemps, temp)
     for μ = 1:dim
         shift_p = ntuple(i -> ifelse(i == μ, 1, 0), dim)
         shift_m = ntuple(i -> ifelse(i == μ, -1, 0), dim)
+        #substitute_U!(Ufat[μ], U[μ])
         shiftedadd(y, U[μ], x, γs[μ], shift_p, shift_m, phi1, phi2, κ)
+        #shiftedadd(y, Ufat[μ], x, γs[μ], shift_p, shift_m, phi1, phi2, κ)
     end
 end
 
@@ -60,6 +121,28 @@ end
 #ψ_n - κ sum_ν U_n[ν](1 + γν)*ψ_{n+ν} + U_{n-ν}[-ν]^+ (1 - γν)*ψ_{n-ν}
 function apply_wilson_dag!(y, U1, U2, U3, U4, x, params, phitemps, temp)
     U = (U1, U2, U3, U4)
+    C = temp[1]
+    D = temp[2]
+    Uout = temp[3]
+    Ufat1 = temp[4]
+    Ufat2 = temp[5]
+    Ufat3 = temp[6]
+    Ufat4 = temp[7]
+    E = temp[8]
+    clear_U!(E)
+    t = params.t
+
+    Ufat = (Ufat1, Ufat2, Ufat3, Ufat4)
+    dim = 4
+    #=
+        for μ = 1:dim
+            shift_μ = ntuple(i -> ifelse(i == μ, 1, 0), dim)
+            make_μloop(Uout, C, D, E, μ, U, shift_μ, dim, t)
+            mul!(Ufat[μ], Uout, U[μ])
+            set_wing_U!(Ufat[μ])
+        end
+
+    =#
 
     clear_fermion!(y)
     add_fermion!(y, 1, x)
@@ -72,7 +155,9 @@ function apply_wilson_dag!(y, U1, U2, U3, U4, x, params, phitemps, temp)
     for μ = 1:dim
         shift_p = ntuple(i -> ifelse(i == μ, 1, 0), dim)
         shift_m = ntuple(i -> ifelse(i == μ, -1, 0), dim)
+        #substitute_U!(Ufat[μ], U[μ])
         shifteddagadd(y, U[μ], x, γs[μ], shift_p, shift_m, phi1, phi2, κ)
+        #shifteddagadd(y, Ufat[μ], x, γs[μ], shift_p, shift_m, phi1, phi2, κ)
     end
 end
 
@@ -89,6 +174,7 @@ function MDtest!(gauge_action, U, Dim, η, ξ, fermi_action, ξcpu, Ucpu,
 
     if length(ARGS) < 1
         cpumode = false
+        println("AD is used")
     else
         cpumode = parse(Bool, ARGS[1])
     end
@@ -108,7 +194,7 @@ function MDtest!(gauge_action, U, Dim, η, ξ, fermi_action, ξcpu, Ucpu,
     Random.seed!(123)
 
 
-    numtrj = 10
+    numtrj = 1000
     for itrj = 1:numtrj
         #@code_warntype MDstep!(gauge_action,U,p,MDsteps,Dim,Uold,fermi_action,η,ξ)
         #error("cc")
@@ -268,8 +354,8 @@ function MDstep!(gauge_action, U, p, MDsteps, Dim, Uold, η, ξ, fermi_action,
 
     Sgold = calc_action(gauge_action, U, p)
     Sgcpuold = calc_action(gauge_action_cpu, Ucpu, pcpu)
-    println_verbose_level2(U[1], "Sfold = $Sfold Sgnew = $Sgold ")
-    println_verbose_level2(U[1], "Sfcpuold = $Sfold Sgcpunew = $Sgcpuold ")
+    println_verbose_level2(U[1], "Sfold = $Sfold Sgold = $Sgold ")
+    println_verbose_level2(U[1], "Sfcpuold = $Sfcpuold Sgcpuold = $Sgcpuold ")
 
     Sold = Sgold + Sfold
     Scpuold = Sgcpuold + Sfcpuold
@@ -403,6 +489,7 @@ function P_update_fermion!(U, p, ϵ, Δτ, Dim, gauge_action, fermi_action, η) 
     UdSfdUμ = temps[1:Dim]
     factor = -ϵ * Δτ
 
+    #clear_U!(UdSfdUμ)
     calc_UdSfdU!(UdSfdUμ, fermi_action, U, η)
 
 
@@ -413,76 +500,152 @@ function P_update_fermion!(U, p, ϵ, Δτ, Dim, gauge_action, fermi_action, η) 
 
 end
 
+function Liederivative(X, μ, indices_halo, U, ϵ, η, fermi_action)
+    Ut = similar(U)
+    substitute_U!(Ut, U)
+    Ui = U[μ].U.A[:, :, indices_halo...]
+    Upi = exp(ϵ * X) * Ui
+    Ut[μ].U.A[:, :, indices_halo...] .= Upi
+    set_wing_U!(Ut[μ])
+
+    Sp = evaluate_FermiAction(fermi_action, Ut, η)
+
+    substitute_U!(Ut, U)
+    Ui = U[μ].U.A[:, :, indices_halo...]
+    Umi = exp(-ϵ * X) * Ui
+    Ut[μ].U.A[:, :, indices_halo...] .= Umi
+    set_wing_U!(Ut[μ])
+
+    Sm = evaluate_FermiAction(fermi_action, Ut, η)
+    dSdU = (Sp - Sm) / (2ϵ)
+    return dSdU
+
+end
+
+#TA(M, NC) = (M - M') / 2 - I * tr((M - M') / NC) / 2
+
+function TA(M, NC)
+    A = (M - M') / 2
+    A = A - (tr(A) / NC) * I
+    return A
+end
+
 function P_update_fermion!(U, p, ϵ, Δτ, Dim, gauge_action, η,
     ηcpu, Ucpu, fermi_action_cpu, gauge_action_cpu, fermi_action)  # p -> p +factor*U*dSdUμ
 
-    #NC = U[1].NC
+    NC = U[1].NC
     temps = get_temporary_gaugefields(gauge_action)
     #UdSfdUμ = temps[1:Dim]
     UdSfdUμ = temps[1:Dim]
     Gaugefields.clear_U!(UdSfdUμ)
     #factor = -ϵ * Δτ
-    factor_ad = -ϵ * Δτ * (-0.5)
+    factor_ad = -ϵ * Δτ #* (-0.5)
 
     #substitute_U!(Ucpu, U)
-    #tempscpu = get_temporary_gaugefields(gauge_action_cpu)
-    #UdSfdUμcpu = tempscpu[1:Dim]
-    #substitute_fermion!(ηcpu, η)
-    #substitute_U!(Ucpu, U)
+    tempscpu = get_temporary_gaugefields(gauge_action_cpu)
+    UdSfdUμcpu = tempscpu[1:Dim]
+    substitute_fermion!(η, ηcpu)
+    substitute_U!(Ucpu, U)
 
     set_wing_U!(U)
-    #set_wing_fermion!(ηcpu)
+    set_wing_U!(Ucpu)
+    set_wing_fermion!(ηcpu)
     set_wing_fermion!(η)
 
 
-    #=
     calc_UdSfdU!(UdSfdUμcpu, fermi_action_cpu, Ucpu, ηcpu)
 
+    NX = U[1].NX
+    NY = U[1].NY
+    NZ = U[1].NZ
+    NT = U[1].NT
+
+    calc_UdSfdU!(UdSfdUμ, fermi_action, U, η)
+    nr = 1000
+    fp = open("fnorm.txt", "w")
+
+    #for k = 1:nr
+    for ix = 1:NX
+        for iy = 1:NY
+            for iz = 1:NZ
+                for it = 1:NT
+                    for μ = 1:4
+                        #ix = rand(1:NX)
+                        #iy = rand(1:NY)
+                        #iz = rand(1:NZ)
+                        #it = rand(1:NT)
+                        indices = (ix, iy, iz, it)
+                        #println(indices)
+
+                        indices_halo = Tuple(collect(indices) .+ 1)
+
+                        #μ = rand(1:4)
+                        UdSfdUμi = UdSfdUμ[μ].U.A[:, :, indices_halo...]
+                        F = TA(UdSfdUμi, NC)
+
+                        UdSfdUμicpu = UdSfdUμcpu[μ].U.A[:, :, indices_halo...]
+                        Fcpu = TA(UdSfdUμicpu, NC)
+                        diff = F - Fcpu
+                        #display(F)
+                        #display(Fcpu)
+                        rel = sqrt(tr(diff' * diff)) / sqrt(tr(Fcpu' * Fcpu))
+                        #println(rel)
+                        println(fp, real(rel))
+                    end
+
+                end
+            end
+        end
+    end
+    close(fp)
+
+
+    #=
     indices = (1, 2, 1, 3)
     indices_halo = Tuple(collect(indices) .+ 1)
 
     dS = zeros(ComplexF64, NC, NC)
-    dScpu = zeros(ComplexF64, NC, NC)
+    #dScpu = zeros(ComplexF64, NC, NC)
     Stest = evaluate_FermiAction(fermi_action, U, η)
-    Stestcpu = evaluate_FermiAction(fermi_action_cpu, Ucpu, ηcpu)
+    #Stestcpu = evaluate_FermiAction(fermi_action_cpu, Ucpu, ηcpu)
     Ut = similar(U)
-    Utcpu = similar(Ucpu)
+    #Utcpu = similar(Ucpu)
 
     for i = 1:NC
         for j = 1:NC
             substitute_U!(Ut, U)
-            substitute_U!(Utcpu, Ucpu)
+            #substitute_U!(Utcpu, Ucpu)
             set_wing_U!(Ut)
-            set_wing_U!(Utcpu)
+            #set_wing_U!(Utcpu)
             eta = 1e-4
             Ut[1].U.A[i, j, indices_halo...] += eta
             set_wing_U!(Ut)
-            Utcpu[1].U[i, j, indices...] += eta
-            set_wing_U!(Utcpu)
+            #Utcpu[1].U[i, j, indices...] += eta
+            #set_wing_U!(Utcpu)
             reStest = evaluate_FermiAction(fermi_action, Ut, η)
-            reStestcpu = evaluate_FermiAction(fermi_action_cpu, Utcpu, ηcpu)
+            #reStestcpu = evaluate_FermiAction(fermi_action_cpu, Utcpu, ηcpu)
 
             substitute_U!(Ut, U)
             set_wing_U!(Ut)
-            substitute_U!(Utcpu, Ucpu)
-            set_wing_U!(Utcpu)
+            #substitute_U!(Utcpu, Ucpu)
+            #set_wing_U!(Utcpu)
             Ut[1].U.A[i, j, indices_halo...] += im * eta
-            Utcpu[1].U[i, j, indices...] += im * eta
+            #Utcpu[1].U[i, j, indices...] += im * eta
             set_wing_U!(Ut)
-            set_wing_U!(Utcpu)
+            #set_wing_U!(Utcpu)
             imStest = evaluate_FermiAction(fermi_action, Ut, η)
-            imStestcpu = evaluate_FermiAction(fermi_action_cpu, Utcpu, ηcpu)
+            #imStestcpu = evaluate_FermiAction(fermi_action_cpu, Utcpu, ηcpu)
             #println((Stest, reStest, imStest))
             dS[i, j] = (reStest - Stest) / eta + im * (imStest - Stest) / eta
-            dScpu[i, j] = (reStestcpu - Stestcpu) / eta + im * (imStestcpu - Stestcpu) / eta
+            #dScpu[i, j] = (reStestcpu - Stestcpu) / eta + im * (imStestcpu - Stestcpu) / eta
         end
     end
 
     println("dS")
-    display(dS)
-    println("dScpu")
-    display(dScpu)
-    =#
+    display(dS .* (-0.5))
+    #println("dScpu")
+    # display(dScpu)
+
     #calc_UdSfdU!(UdSfdUμcpu, fermi_action_cpu, Ucpu, ηcpu)
     #substitute_U!(UdSfdUμ, UdSfdUμcpu)
     calc_UdSfdU!(UdSfdUμ, fermi_action, U, η)
@@ -490,24 +653,80 @@ function P_update_fermion!(U, p, ϵ, Δτ, Dim, gauge_action, η,
     #display(Ucpu[1].U[:, :, 2, 2, 2, 2]' * UdSfdUμcpu[1].U[:, :, 2, 2, 2, 2])
     #println("cpu")
 
-    #=
-    display(UdSfdUμcpu[1].U[:, :, indices...])
+
+    #display(UdSfdUμcpu[1].U[:, :, indices...])
     display(UdSfdUμ[1].U.A[:, :, indices_halo...])
     println("U")
     display(U[1].U.A[:, :, indices_halo...])
     println("UdU")
     display(U[1].U.A[:, :, indices_halo...]' * UdSfdUμ[1].U.A[:, :, indices_halo...])
-    println("Ucpu")
-    display(Ucpu[1].U[:, :, indices...])
+    #println("Ucpu")
+    #display(Ucpu[1].U[:, :, indices...])
     println(tr(UdSfdUμ[1]))
-    println(tr(UdSfdUμcpu[1]))
+    #println(tr(UdSfdUμcpu[1]))
 
-    udsfdu = UdSfdUμcpu[1].U[:, :, indices...]
-    u = Ucpu[1].U[:, :, indices...]
-    println("udag udsfdu")
-    display(u' * udsfdu)
+    #udsfdu = UdSfdUμcpu[1].U[:, :, indices...]
+    #u = Ucpu[1].U[:, :, indices...]
+    #println("udag udsfdu")
+    #display(u' * udsfdu)
     error("d")
+
     =#
+
+    #=
+    σ = sqrt(1 / 2)
+
+    NX = U[1].NX
+    NY = U[1].NY
+    NZ = U[1].NZ
+    NT = U[1].NT
+
+
+    calc_UdSfdU!(UdSfdUμ, fermi_action, U, η)
+    nr = 10
+    ϵ = 1e-4
+    ϵs = [1e-2, 1e-3, 1e-4, 1e-5, 1e-6]
+    fp2 = open("liederiv.txt", "w")
+    for ϵ in ϵs
+        println("ϵ = ", ϵ)
+        fp = open("liederiv_$ϵ.txt", "w")
+        for k = 1:nr
+            ix = rand(1:NX)
+            iy = rand(1:NY)
+            iz = rand(1:NZ)
+            it = rand(1:NT)
+            indices = (ix, iy, iz, it)
+            println(indices)
+
+            indices_halo = Tuple(collect(indices) .+ 1)
+
+            X = σ * randn(NC, NC) + im * σ * randn(NC, NC)
+            X = TA(X, NC)
+            X = X ./ sqrt(tr(X' * X))
+            μ = rand(1:4)
+
+            UdSfdUμi = UdSfdUμ[μ].U.A[:, :, indices_halo...]
+            F = TA(UdSfdUμi, NC)
+            d = real(tr(F * X)) * (-2)
+            println("Lie AD: ", d)
+
+
+            dSdUn = Liederivative(X, μ, indices_halo, U, ϵ, η, fermi_action)
+            println("Lie: ", dSdUn)
+            println("Lie AD: ", d, " Numerical ", dSdUn)
+            println(fp, d, "\t", dSdUn, "\t", abs(d - dSdUn) / abs(d))
+            println(fp2, ϵ, "\t", d, "\t", dSdUn, "\t", abs(d - dSdUn) / abs(d))
+
+        end
+        close(fp)
+    end
+    close(fp2)
+
+    =#
+
+
+
+    error("dd")
 
 
     #display(U[1].U.A[:, :, 3, 3, 3, 3]' * UdSfdUμ[1].U.A[:, :, 2+1, 2+1, 2+1, 2+1])
@@ -539,10 +758,10 @@ end
 
 function test1()
     MPI.Init()
-    NX = 4
-    NY = 4
-    NZ = 4
-    NT = 4
+    NX = 16
+    NY = 16
+    NZ = 16
+    NT = 16
     Nwing = 1
     Dim = 4
     NC = 3
@@ -588,11 +807,12 @@ function test1()
     #substitute_fermion!(x, xcpu)
 
     κ = 0.141139
-    parameters = (κ=κ,)
+    t = 0.1#0.0001
+    parameters = (κ=κ, t=t)
     apply_D(y, U1, U2, U3, U4, x, phitemp, temp) = apply_wilson!(y, U1, U2, U3, U4, x, parameters, phitemp, temp)
     apply_Ddag(y, U1, U2, U3, U4, x, phitemp, temp) = apply_wilson_dag!(y, U1, U2, U3, U4, x, parameters, phitemp, temp)
 
-    fermi_action = GeneralFermionAction(U, x, apply_D, apply_Ddag)
+    fermi_action = GeneralFermionAction(U, x, apply_D, apply_Ddag; numtemp=8, eps_CG=1e-15)
 
     #DdagD = DdagDgeneral(U, x, apply_D, apply_Ddag)
 
@@ -601,7 +821,7 @@ function test1()
     params = Dict()
     params["Dirac_operator"] = "Wilson"
     params["κ"] = κ
-    params["eps_CG"] = 1.0e-12
+    params["eps_CG"] = 1.0e-18
     params["improved gpu"] = false
     #params["improved gpu"] = true
     #params["eps_CG"] = 1.0e-1
@@ -618,6 +838,7 @@ function test1()
     x = GeneralFermion(NC, NG, gsize, PEs; nw, elementtype)
     xcpu = Initialize_pseudofermion_fields(Ucpu[1], "Wilson")
     gauss_distribution_fermion!(xcpu)
+    gauss_distribution_fermion!(x)
     #substitute_fermion!(x, xcpu)
 
     mul!(ycpu, Dcpu, xcpu)
@@ -631,7 +852,7 @@ function test1()
     end
     temp = typeof(U[1])[]
     dtemp = typeof(U[1])[]
-    for i = 1:5
+    for i = 1:8
         push!(temp, similar(U[1]))
         push!(dtemp, similar(U[1]))
     end
